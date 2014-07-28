cee56483df7895b468f56f264345c249a7312e04
Carlos Torchia <carlos@trendlier.com>
2014-07-28 13:16:25 -0700

    make only two select queries to update friends list
    1) Retrieve list of existing facebook users for each of the new friends.
    2) Retrieve list of existing friend associations for the given user.
    
    The method then proceeds to adding any new records if necessary, and removing
    old ones (or setting is_deleted = true) that aren't in the current list.

--- app/serum/dao/FacebookUserDao.java
+++ app/serum/dao/FacebookUserDao.java
@@ -36,31 +36,29 @@ public class FacebookUserDao
         return facebookUser;
     }
 
-    protected static void createUpdateFacebookUserFriend(FacebookUser facebookUser, FacebookUser facebookUserOfFriend)
+    protected static FacebookUser createFacebookUserOfFriend(Facebook.User userFb)
     {
-        FacebookUserFriend facebookUserFriend =
-            Ebean.find(FacebookUserFriend.class)
-            .where().and(
-                Expr.and(
-                    Expr.eq("facebookUser", facebookUser),
-                    Expr.eq("facebookUserOfFriend", facebookUserOfFriend)),
-                Expr.eq("isDeleted", false))
-            .findUnique();
-        if (facebookUserFriend == null)
-        {
-            facebookUserFriend = new FacebookUserFriend(facebookUser, facebookUserOfFriend);
-            Ebean.save(facebookUserFriend);
-        }
+        FacebookUser facebookUser = new FacebookUser();
+        facebookUser.idFacebook = userFb.getId();
+        facebookUser.name = userFb.getName();
+        Ebean.save(facebookUser);
+        return facebookUser;
     }
 
-    protected static void removeUpdateFacebookUserFriends(FacebookUser facebookUser, Facebook.User userFb)
+    protected static Set<String> getFacebookFriendIds(Facebook.User userFb)
     {
+        // Generate the list of Facebook IDs that should be in the friends list
         Set<String> facebookFriendIds = new HashSet<String>();
         for (Facebook.User userFbOfFriend: userFb.getFriends())
         {
             facebookFriendIds.add(userFbOfFriend.getId());
         }
+        return facebookFriendIds;
+    }
 
+    protected static Map<String, FacebookUserFriend> getFacebookUserFriendMap(FacebookUser facebookUser)
+    {
+        // Get the list of facebookUsers that are currently in the friends list
         List<FacebookUserFriend> facebookUserFriendList =
             Ebean.find(FacebookUserFriend.class)
             .fetch("facebookUserOfFriend")
@@ -68,29 +66,61 @@ public class FacebookUserDao
                 Expr.eq("facebookUser", facebookUser),
                 Expr.eq("isDeleted", false))
             .findList();
+        // Convert this list to a map of Facebook ID to Facebook-user-friend object for quick lookup.
+        Map<String, FacebookUserFriend> facebookUserFriendMap = new HashMap<String, FacebookUserFriend>();
         for (FacebookUserFriend facebookUserFriend: facebookUserFriendList)
         {
-            // Remove facebook-user-friend record if it shouldn't exist
-            if (!facebookFriendIds.contains(facebookUserFriend.facebookUserOfFriend.idFacebook))
-            {
-                facebookUserFriend.isDeleted = true;
-                facebookUserFriend.deletedUTC = Calendar.getInstance(TimeZone.getTimeZone("Etc/UTC"));
-                Ebean.save(facebookUserFriend);
-            }
+            facebookUserFriendMap.put(facebookUserFriend.facebookUserOfFriend.idFacebook, facebookUserFriend);
         }
+        return facebookUserFriendMap;
+    }
+
+    protected static Map<String, FacebookUser> getFacebookUserMapByIds(Set<String> facebookIds)
+    {
+        // Get the list of facebookUsers that are currently in the friends list
+        List<FacebookUser> facebookUserList =
+            Ebean.find(FacebookUser.class)
+            .where().and(
+                Expr.in("idFacebook", facebookIds),
+                Expr.eq("isDeleted", false))
+            .findList();
+        // Convert this list to a map of Facebook ID to Facebook user object for quick lookup.
+        Map<String, FacebookUser> facebookUserMap = new HashMap<String, FacebookUser>();
+        for (FacebookUser facebookUser: facebookUserList)
+        {
+            facebookUserMap.put(facebookUser.idFacebook, facebookUser);
+        }
+        return facebookUserMap;
     }
 
     public static void createUpdateFacebookUserFriends(FacebookUser facebookUser, Facebook.User userFb)
     {
-        if (userFb.getFriends() == null)
+        Set<String> facebookFriendIds = getFacebookFriendIds(userFb);
+        Map<String, FacebookUserFriend> existingFacebookFriendMap = getFacebookUserFriendMap(facebookUser);
+        Map<String, FacebookUser> existingFacebookUserOfFriendMap = getFacebookUserMapByIds(facebookFriendIds);
+        // Remove each friend that is currently in the old list but not in the new list.
+        for (String idFacebook: existingFacebookFriendMap.keySet())
         {
-            return;
+            if (!facebookFriendIds.contains(idFacebook))
+            {
+                FacebookUserFriend facebookUserFriend = existingFacebookFriendMap.get(idFacebook);
+                facebookUserFriend.isDeleted = true;
+                Ebean.save(facebookUserFriend);
+            }
         }
+        // Add each friend that currently is in the new list but not in the old list.
         for (Facebook.User userFbOfFriend: userFb.getFriends())
         {
-            FacebookUser facebookUserOfFriend = createUpdateFacebookUser(userFbOfFriend);
-            createUpdateFacebookUserFriend(facebookUser, facebookUserOfFriend);
+            if (!existingFacebookFriendMap.containsKey(userFbOfFriend.getId()))
+            {
+                FacebookUser facebookUserOfFriend = existingFacebookUserOfFriendMap.get(userFbOfFriend.getId());
+                if (facebookUserOfFriend == null)
+                {
+                    facebookUserOfFriend = createFacebookUserOfFriend(userFbOfFriend);
+                }
+                FacebookUserFriend facebookUserFriend = new FacebookUserFriend(facebookUser, facebookUserOfFriend);
+                Ebean.save(facebookUserFriend);
+            }
         }
-        removeUpdateFacebookUserFriends(facebookUser, userFb);
     }
 }
