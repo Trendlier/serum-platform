1e7eb8cc4a2d1061a81331c47defdd72d7d0f994
Carlos Torchia <carlos@trendlier.com>
2014-07-28 13:29:05 -0700

    add empty test case for functional/system test to test login service

---
+++ test/serum/controller/LoginControllerTest.java
@@ -0,0 +1,10 @@
+package serum.controller;
+
+/**
+ * TODO: When someone gets time, add functional tests (Google 'play framework test').
+ * The tests should generate a request to the server for different facebook users.
+ * However, this would require an active facebook access token be entered every time.
+ */
+public class LoginControllerTest
+{
+}
