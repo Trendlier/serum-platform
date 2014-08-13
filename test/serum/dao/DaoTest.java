2d478f2f9ea253342346af1c2ac2cf7853bfb69c
Carlos Torchia <carlos@trendlier.com>
2014-08-13 10:38:25 -0700

    separate unit test transactions into AOP-style Junit Rule

--- test/serum/dao/DaoTest.java
+++ test/serum/dao/DaoTest.java
@@ -5,43 +5,23 @@ import java.util.*;
 import org.junit.*;
 import play.test.*;
 
-import play.db.jpa.*;
-import javax.persistence.*;
-
 public class DaoTest
 {
     private static FakeApplication app;
-    private static EntityManager em;
 
-    protected EntityTransaction txn;
+    @Rule
+    public final DaoTestRule daoTestRule = new DaoTestRule();
 
     @BeforeClass
     public static void setUpApp()
     {
         app = Helpers.fakeApplication();
         Helpers.start(app);
-        em = JPA.em("default");
-        JPA.bindForCurrentThread(em);
-    }
-
-    @Before
-    public void setUpTransaction()
-    {
-        txn = em.getTransaction();
-        txn.begin();
-    }
-
-    @After
-    public void tearDownTransaction()
-    {
-        txn.commit();
     }
 
     @AfterClass
     public static void tearDownApp()
     {
-        JPA.bindForCurrentThread(null);
-        em.close();
         Helpers.stop(app);
     }
 }
---
+++ test/serum/dao/DaoTestRule.java
@@ -0,0 +1,52 @@
+package serum.dao;
+
+import org.junit.rules.MethodRule;
+import org.junit.runners.model.Statement;
+import org.junit.runners.model.FrameworkMethod;
+
+import play.db.jpa.*;
+import javax.persistence.*;
+
+public class DaoTestRule implements MethodRule
+{
+    private EntityManager em;
+    protected EntityTransaction txn;
+
+    public EntityTransaction getTransaction()
+    {
+        return txn;
+    }
+
+    @Override
+    public Statement apply(final Statement statement, FrameworkMethod method, Object test)
+    {
+        return new Statement() {
+            @Override
+            public void evaluate() throws Throwable
+            {
+                em = JPA.em("default");
+                JPA.bindForCurrentThread(em);
+                txn = em.getTransaction();
+                txn.begin();
+                try
+                {
+                    statement.evaluate();
+                    txn.commit();
+                }
+                catch (Throwable t)
+                {
+                    try { txn.rollback(); } catch(Throwable t2) {}
+                    throw t;
+                }
+                finally
+                {
+                    JPA.bindForCurrentThread(null);
+                    if (em != null)
+                    {
+                        em.close();
+                    }
+                }
+            }
+        };
+    }
+}
